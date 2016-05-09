f4c8d38ff9372c7084052e6963f5a39245fd1250
Ryan McCue <me@ryanmccue.info>
2016-05-09 21:55:24 +1000

    Add masthead to home page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,22 @@
 <?php get_header(); the_post(); ?>
 
+
+	<div class="masthead">
+		<p>This site is a central application registry (&ldquo;broker&rdquo;) for WordPress REST API applications using OAuth 1.</p>
+
+		<nav>
+			<?php if ( is_user_logged_in() ) : ?>
+				<a href="<?php echo home_url( '/apps/' ) ?>">View your Applications</a>
+			<?php else : ?>
+				<a href="<?php echo wp_login_url( home_url( '/apps/' ) ) ?>">Log In to Register Applications</a>
+			<?php endif ?>
+		</nav>
+
+		<p class="additional">
+			<a href="">Information for Application Developers</a>
+		</p>
+	</div>
+
 <div class="content">
 	<?php the_content() ?>
 </div>
