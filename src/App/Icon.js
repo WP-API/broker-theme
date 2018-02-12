b95ce6e4d2826369e593e348e4573261f02f5831
Ryan McCue <me@ryanmccue.info>
2018-02-12 17:43:50 +1000

    Use featured media as icon

--- src/App/Icon.js
+++ src/App/Icon.js
@@ -6,8 +6,9 @@ export default function AppIcon( props ) {
 	const { app } = props;
 
 	let url;
-	if ( app.icon && app.icon.url ) {
-		url = app.icon.url;
+	if ( app.featured_media && app._embedded && app._embedded['wp:featuredmedia'] ) {
+		const featured = app._embedded['wp:featuredmedia'][0];
+		url = `url( ${ featured.media_details.sizes.thumbnail.source_url } )`;
 	} else {
 		const pattern = window.GeoPattern.generate( app.title.rendered );
 		url = pattern.toDataUrl();
