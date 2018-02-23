72f4be30374b90f2fee57cf17d58196c26c9e629
Ryan McCue <me@ryanmccue.info>
2018-02-23 14:54:08 +1000

    Add logo from dashicons

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Logo from './Logo';
+
 import './Header.css';
 
 export default function Header( props ) {
 	const { children, title } = props;
 
 	return <header className="Header">
-		<h2>{ title }</h2>
+		<h2><Logo light /> { title }</h2>
 
 		<ul className="Header-links">
 			{ children }
